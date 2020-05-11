354da692304a8593ac90a9288bcface6d9590781
Maciej Grzybek <maciekgrzybek1@gmail.com>
2020-05-11 20:13:13 +0100

    Cleanup, add methods

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,38 +1,54 @@
 <script>
-  import { createEventDispatcher, onMount } from 'svelte';
+  import { createEventDispatcher, onMount } from "svelte";
 
   let wrapper;
-  export let inView = false;
+  let observe;
+  let unobserve;
+  let entry;
+
+  let inView = false;
+
   const dispatch = createEventDispatcher();
 
   onMount(() => {
-    if(typeof IntersectionObserver !== 'undefined') {
-      console.log(wrapper)
+    if (typeof IntersectionObserver !== "undefined") {
       const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-          dispatch('intersecting', entry)
-          if(entry.isIntersecting) {
+        entries.forEach((singleEntry) => {
+          dispatch("intersecting", singleEntry);
+          entry = singleEntry;
+          if (entry.isIntersecting) {
             inView = true;
           } else {
             inView = false;
           }
-        })
+        });
+      });
 
-      })
-      
+      observe = observer.observe;
+      unobserve = observer.unobserve;
+
       observer.observe(wrapper);
       return () => observer.unobserve(wrapper);
     }
-  })
+  });
 </script>
 
 <style>
   div {
-    width: 100px;
-    height: 10px;
+    width: 100%;
+    height: 100%;
   }
 </style>
 
 <div bind:this={wrapper}>
-  <slot {inView}/>
+  <slot {inView} {observe} {unobserve} {entry} />
 </div>
+
+<!-- +++++++++++++++++++++++++++++++++++++++++++++++++ TODO: expose inView and IO Entry(with entry info) props +++++++++++++++++++++++++++++++++++++++++++++++++-->
+<!-- TODO: pass IO props-->
+<!-- TODO: expose add events -> onChange, onEnter, onLeave-->
+<!-- TODO: add scrollDirection detection -->
+<!-- TODO: add unobserveOnEnter -->
+<!-- +++++++++++++++++++++++++++++++++++++++++++++++++ TODO: expose observe and unObserve methods ++++++++++++++++++++++++++++++++++++++++++++++++++-->
+<!-- TODO: in future handle IO v2 -->
+<!-- TODO: add demo and examples in readme -->
