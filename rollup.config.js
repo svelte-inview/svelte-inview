9bb575a56ec336f9e564dbcb6fe680e6006e1bac
Jonas Pöppelmann <jonas.poeppelmann@gmail.com>
2021-05-23 19:10:45 +0200

    Expose type definitions in package.json
    I replaced @rollup/plugin-typescript with rollup-plugin-typescript2 for
    this to work.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import autoPreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
 import pkg from './package.json';
 
