f3e980a9f0d6eaed57aa7cb509cd028d0d5c3cf8
Maciej Grzybek <maciekgrzybek1@gmail.com>
2023-02-25 08:10:51 +0100

    chore: remove netlify from the test app

--- test-app/.netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-a3782f6c.js';
-export const imports = ["_app/immutable/components/error.svelte-a3782f6c.js","_app/immutable/chunks/index-44dfb30b.js","_app/immutable/chunks/singletons-23acf279.js"];
-export const stylesheets = [];
-export const fonts = [];
--- test-app/svelte.config.js
+++ test-app/svelte.config.js
@@ -1,12 +1,8 @@
-import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
+	preprocess: vitePreprocess()
 };
 
 export default config;
